111d50eabab285d3a78aea9a9244fde7b87cc6cd
Giulio Russo <russogiulio1998@icloud.com>
2025-02-23 09:14:46 +0100

    Update draw

--- nidataset/Draw.py
+++ nidataset/Draw.py
@@ -1,26 +1,41 @@
 import numpy as np
 import nibabel as nib
 import torch
+import os
 
 
 def draw_boxes_on_nifti(tensor: torch.Tensor,
-                        nifti_file_path: str,
+                        nii_path: str,
+                        output_path: str,
                         intensity_based_on_score: bool = False,
                         debug: bool = False) -> None:
     """
-    Draws 3D bounding boxes on a nii.gz file based on the provided tensor and saves a new nii.gz file.
+    Draws 3D bounding boxes on a nii.gz file based on the provided tensor and saves a new nii.gz file inside the specified output path.
 
-    :param tensor: A tensor containing columns ['SCORE', 'X MIN', 'Y MIN', 'Z MIN', 'X MAX', 'Y MAX', 'Z MAX'] where XYZ are already in the 3D reference system.
-    :param nifti_file_path: Path to the original nii.gz file.
+    :param tensor: A tensor containing columns with the following structure: ['SCORE', 'X MIN', 'Y MIN', 'Z MIN', 'X MAX', 'Y MAX', 'Z MAX'] where XYZ are already in the 3D reference system.
+    :param nii_path: Path to the original nii.gz file.
+    :param output_path: Output path.
     :param intensity_based_on_score: If True, use the 'SCORE' column for box intensity with steps. Otherwise, use intensity 1.
     :param debug: if True, prints additional information about the draw.
 
-    Returns:
-    None, saves a new nii.gz file with drawn boxes.
+    :raises FileNotFoundError: if the dataset folder does not exist or contains no .nii.gz files.
+    :raises ValueError: if an invalid view or saving_mode is provided.
     """
+
+    # check if the input file exists
+    if not os.path.isfile(nii_path):
+        raise FileNotFoundError(f"Error: the input file '{nii_path}' does not exist.")
+
+    # ensure the file is a .nii.gz file
+    if not nii_path.endswith(".nii.gz"):
+        raise ValueError(f"Error: invalid file format. Expected a '.nii.gz' file, but got '{nii_path}'.")
+
+    # create output dir if it does not exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     
     # load the nii.gz file
-    nifti_image = nib.load(nifti_file_path)
+    nifti_image = nib.load(nii_path)
     affine = nifti_image.affine
 
     # create a new data array for output
@@ -47,23 +62,25 @@ def draw_boxes_on_nifti(tensor: torch.Tensor,
 
     # create a new Nifti image
     new_nifti_image = nib.Nifti1Image(output_data, affine)
-    new_file_path = nifti_file_path.replace('.nii.gz', '_with_boxes.nii.gz')
-    nib.save(new_nifti_image, new_file_path)
+    new_nifti_filename = os.path.basename(nii_path).replace('.nii.gz', '_with_boxes.nii.gz')
+    nii_output_path =  os.path.join(output_path, new_nifti_filename)
+    nib.save(new_nifti_image, nii_output_path)
 
     if debug:
-        print(f"New nii.gz file saved at: {new_file_path}")
+        print(f"New nii.gz file saved at: {nii_output_path}")
 
 
-def from_2D_to_3D_coords(tensor: torch.Tensor, view: str) -> torch.Tensor:
+def from_2D_to_3D_coords(tensor: torch.Tensor,
+                         view: str) -> torch.Tensor:
     """
     Switches the box coordinates in the tensor based on the specified anatomical view.
 
-    Args:
-    tensor (torch.Tensor): A tensor with columns ['X MIN', 'Y MIN', 'SLICE NUMBER MIN', 'X MAX', 'Y MAX', 'SLICE NUMBER MAX'] or ['X', 'Y', 'SLICE NUMBER'].
-    view (str): The view from which to adjust the coordinates ('axial', 'coronal', 'sagittal').
+    :param tensor: A tensor containing columns with the following structure: ['X MIN', 'Y MIN', 'SLICE NUMBER MIN', 'X MAX', 'Y MAX', 'SLICE NUMBER MAX'] or ['X', 'Y', 'SLICE NUMBER'].
+    :param view: The view from which to adjust the coordinates ('axial', 'coronal', 'sagittal').
 
-    Returns:
-    torch.Tensor: The tensor with adjusted coordinates.
+    :return result: The tensor with adjusted coordinates.
+
+    :raises ValueError: if an invalid number of columns is provided inside the input tensor.
     """
 
     if tensor.shape[1] not in (3, 6):
